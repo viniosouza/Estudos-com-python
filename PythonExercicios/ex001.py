63b74a98cf8c902ae1e03f3d327eea677f9e6d63
Vinicios Oliveira Souza <violivi@davo.com.br>
2020-11-20 12:45:09 -0300

    first commit

---
+++ PythonExercicios/ex001.py
@@ -0,0 +1,6 @@
+#Primeiro comando - Desafio - 01
+# print('Olá mundo, meu primeiro programa em python')
+
+msg = 'Olá mundo, meu primeiro programa em python'
+
+print(msg)
---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,5 @@
+name = input('Digite seu nome: ')
+# print('É um prazer te conhecer', name, '!!!!')
+
+print('É um prazer te conhecer {}, seja bem vindo!!'.format(name))
+
